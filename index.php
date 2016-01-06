eec489fad401832447b440410e2248ec87045a39
Aicha Hezit <A00906732@ad.bcit.ca>
2016-01-06 11:08:44 -0800

    Seventh Step: game logic errors

--- index.php
+++ index.php
@@ -14,45 +14,28 @@
             $squares = str_split($position);
             
             function winner($token,$position) {
-                $won = false;
-                if (($position[0] == $token) &&
-                ($position[1] == $token) &&
-                ($position[2] == $token)) {
-                    $won = true;
-                } else if (($position[3] == $token) &&
-                ($position[4] == $token) &&
-                ($position[5] == $token)) {
-                    $won = true;
-                }else if (($position[6] == $token) &&
-                ($position[7] == $token) &&
-                ($position[8] == $token)) {
-                    $won = true;
-                }else if (($position[0] == $token) &&
-                ($position[4] == $token) &&
-                ($position[8] == $token)) {
-                    $won = true;
-                }else if (($position[2] == $token) &&
+                // !!game logic errors!!
+                for($row=0; $row<3; $row++) {
+                    $result = true;
+                    for($col=0; $col<3; $col++){
+                        if ($position[3*$row+$col] != $token) $result = false; // note the negative test
+                    }
+                }
+                // checks for diagonals
+                if (($position[2] == $token) &&
                 ($position[4] == $token) &&
                 ($position[6] == $token)) {
-                    $won = true;
+                    $result = true;
                 }else if (($position[0] == $token) &&
                 ($position[3] == $token) &&
                 ($position[6] == $token)) {
-                    $won = true;
-                }else if (($position[1] == $token) &&
-                ($position[4] == $token) &&
-                ($position[7] == $token)) {
-                    $won = true;
-                }else if (($position[2] == $token) &&
-                ($position[5] == $token) &&
-                ($position[8] == $token)) {
-                    $won = true;
+                    $result = true;
                 }
-                return $won;
+                return $result;
             }
             
-            if (winner('x',$squares)) echo 'You win.';
-            else if (winner('o',$squares)) echo 'I win.';
+            if (winner('x',$squares)) echo 'X wins.';
+            else if (winner('o',$squares)) echo 'Y wins.';
             else echo 'No winner yet.';
         ?>
     </body>

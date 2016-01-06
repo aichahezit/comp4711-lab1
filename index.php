9fa0ea9f432d7a245c66a036640534ffd21a8fa5
Aicha Hezit <A00906732@ad.bcit.ca>
2016-01-06 11:46:30 -0800

    Eighth Step: game class added

--- index.php
+++ index.php
@@ -6,37 +6,64 @@
     </head>
     <body>
         <?php
+      
             if (!isset($_GET['board'])){
                 die("Enter board parameters!");
             }
-            
-            $position = $_GET['board'];
-            $squares = str_split($position);
-            
-            function winner($token,$position) {
-                // !!game logic errors!!
-                for($row=0; $row<3; $row++) {
-                    $result = true;
-                    for($col=0; $col<3; $col++){
-                        if ($position[3*$row+$col] != $token) $result = false; // note the negative test
+        
+            class Game{
+                var $position;
+                
+                function __construct($squares) {
+                    $this->position = str_split($squares);
+                }
+                
+                //this doesn't work..........
+                function winner($token){
+                     // !!game logic errors, doesn't check vertically
+                    for($row=0; $row<3; $row++) {
+                        $result = true;
+                        for($col=0; $col<3; $col++){
+                            if ($this->position[3*$row+$col] != $token) $result = false; // note the negative test
+                        }
+                        if ($result){
+                            return $result;
+                        }
                     }
+                    
+                    for($col=0; $col<3; $col++) {
+                        $result = true;
+                        for($row=0; $row<3; $row++){
+                            if ($this->position[3*$col+$row] != $token) $result = false; // note the negative test
+                        }
+                        if ($result){
+                            return $result;
+                        }
+                    }
+                 
+                    // checks for diagonals
+                    if (($this->position[2] == $token) &&
+                    ($this->position[4] == $token) &&
+                    ($this->position[6] == $token)) {
+                        $result = true;
+                    }else if (($this->position[0] == $token) &&
+                    ($this->position[3] == $token) &&
+                    ($this->position[6] == $token)) {
+                        $result = true;
+                    }
+                    return $result;
                 }
-                // checks for diagonals
-                if (($position[2] == $token) &&
-                ($position[4] == $token) &&
-                ($position[6] == $token)) {
-                    $result = true;
-                }else if (($position[0] == $token) &&
-                ($position[3] == $token) &&
-                ($position[6] == $token)) {
-                    $result = true;
-                }
-                return $result;
             }
             
-            if (winner('x',$squares)) echo 'X wins.';
-            else if (winner('o',$squares)) echo 'Y wins.';
-            else echo 'No winner yet.';
+            $squares = $_GET['board'];
+            $game = new Game($squares);
+            if ($game->winner('x'))
+            echo 'X wins. Lucky guesses!';
+            else if ($game->winner('o'))
+            echo 'Y wins. Muahahahaha';
+            else
+            echo 'No winner yet, but you are losing.';
+            
         ?>
     </body>
 </html>
